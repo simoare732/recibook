03a8c21ade65f926896031179f1b1d77d606ff76
simoare732 <simone.aresta03@gmail.com>
2025-01-23 21:45:12 +0100

    Added navbar in homepage

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'pages',
     'recipe',
 ]
@@ -118,9 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- project/recipe/forms.py
+++ project/recipe/forms.py
@@ -1,3 +1,89 @@
 from django import forms
 from .models import *
 
+class recipeForm(forms.ModelForm):
+    name = forms.CharField(
+        label='Titolo',
+        required=True,
+        max_length=255,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    preparation = forms.CharField(
+        label='Preparazione',
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder':'step 1: ... \nstep 2: ...'})
+    )
+    category = forms.ModelChoiceField(
+        label='Categoria',
+        required=True,
+        queryset=Category.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    image = forms.ImageField(
+        label='Immagine',
+        widget=forms.FileInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ['name', 'category',  'image', 'preparation']
+
+
+class recipeIngredientForm(forms.ModelForm):
+    ingredient = forms.ModelChoiceField(
+        label='Ingrediente',
+        required=True,
+        queryset=Ingredient.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    quantity = forms.CharField(
+        label='Quantità',
+        required=True,
+        max_length=10,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    unit_of_measure = forms.ChoiceField(
+        label='Unità di misura',
+        required=True,
+        choices=RecipeIngredient.UNIT,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ['ingredient', 'quantity', 'unit_of_measure']
+
+
+class ingredientForm(forms.ModelForm):
+    name = forms.CharField(
+        label='Nome',
+        required=True,
+        max_length=255,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = Ingredient
+        fields = ['name']
+
+class categoryForm(forms.ModelForm):
+    name = forms.CharField(
+        label='Nome',
+        required=True,
+        max_length=255,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = Category
+        fields = ['name']
+
+class noteForm(forms.ModelForm):
+    text = forms.CharField(
+        label='Note',
+        required=False,
+        widget=forms.Textarea(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = Note
+        fields = ['text']
